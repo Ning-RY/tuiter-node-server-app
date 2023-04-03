706131a01347b1d8d1d5a77059f05b8d15088f8c
lnyrlyed <yang.ruini@northeastern.edu>
2023-04-03 17:35:58 -0400

    a9 Mongo sp23

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import HelloController from "./controllers/hello-controller.js"
-import UserController from "./controllers/users/users-controller.js"
+import express from 'express';
+import cors from 'cors';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors())
 TuitsController(app);
-HelloController(app)
-UserController(app)
+HelloController(app);
+UserController(app);
 app.listen(process.env.PORT || 4000);
